6e7aee53df954007745499c64bcb8f247eccb09f
YuukiMorp8 <kuthykymaell@gmail.com>
2025-10-02 19:26:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import os
 
 app = Flask(__name__)
