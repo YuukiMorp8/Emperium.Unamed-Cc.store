774dd64b9ba870aeabc194b6069aa9fa3ba84cd5
YuukiMorp8 <kuthykymaell@gmail.com>
2025-10-03 10:08:04 -0300

    Update pagamento.py

--- pagamento.py
+++ pagamento.py
@@ -5,7 +5,7 @@ import base64
 import os
 from efipay import EfiPay
 
-CERT_PATH = os.path.join(os.path.dirname(__file__), "produção-unamedstore.pem")
+CERT_PATH = os.path.join(os.path.dirname(__file__), "prod store.pem")
 
 credentials = {
     'client_id': os.environ.get("EFI_CLIENT_ID"),
